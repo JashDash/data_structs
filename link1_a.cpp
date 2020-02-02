d24f77f45c079d5763007e5e06bbb93464bc416a
Jashan Devgan <jashandevgan@gmail.com>
2020-02-02 22:49:44 +0530

    Node insertion at end

---
+++ link1_a.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+struct Node{    //can be placed in a class
+    int data;
+    Node* link;
+};
+ void insert(int);
+ void print();
+Node* head; //global head so that it can be used without passing as an argument
+int main(){
+    head = NULL;
+    int N;
+    cout<<"Enter the no of elements in the list: ";
+    cin>>N;
+    for(int i = 0; i<N; i++){
+        int x;
+        cout<<"Enter element: ";
+        cin>>x;
+        insert(x);
+        print();
+        cout<<'\n';
+
+    }
+    
+    return 0;
+}
+void insert(int x){
+    Node* temp = new Node();
+    temp->data = x;
+    temp->link = NULL;
+    if(head == NULL) head = temp;
+    else{
+        Node* temp1 = head;
+        while(temp1->link != NULL){
+            temp1 = temp1->link;
+        } 
+        temp1->link = temp; 
+    } 
+     //link points to where the head is pointing i.e. first element
+    //head = temp;    //new head of list
+}
+void print(){
+    Node* temp = head;
+    while(temp != NULL){    //will become when last element reached since last link = NULL
+        cout<<temp->data<<" ";
+        temp = temp->link;
+    }
+}
